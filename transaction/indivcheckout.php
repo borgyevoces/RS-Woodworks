6349b3c2f0f941a103df7063335f0003c66a95ea
borgyevoces <158753724+borgyevoces@users.noreply.github.com>
2025-02-25 14:33:20 -0800

    Update indivcheckout.php

--- transaction/indivcheckout.php
+++ transaction/indivcheckout.php
@@ -110,7 +110,7 @@ try {
         'headers' => [
             'Content-Type' => 'application/json',
             'accept' => 'application/json',
-            'authorization' => 'Basic ' . base64_encode('sk_test_9V5nRYTWwwFVTBxFwhtZwYJ3'),
+            'authorization' => 'Basic ' . base64_encode(''),
         ],
     ]);
 
